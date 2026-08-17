48c6b51519889448de9baa61a40addc861dd781a
s4Amrit <amrit610pal@gmail.com>
2026-08-17 11:27:24 +0530

    Delete Hyprland Lua directory

--- Hyprland Lua/env.lua
+++
@@ -1,9 +0,0 @@
--- Environment Variables
-hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
-hl.env("LIBVA_DRIVER_NAME", "nvidia")
-hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
-hl.env("NVD_BACKEND", "direct")
-hl.env("XCURSOR_THEME", "Bibata-Modern-Classic")
-hl.env("XCURSOR_SIZE", "24")
-hl.env("HYPRCURSOR_SIZE", "24")
-hl.env("AQ_DRM_DEVICES", "/dev/dri/amd-igpu")
--- Hyprland Lua/exec.lua
+++
@@ -1,7 +0,0 @@
--- Autostart
-hl.on("hyprland.start", function()
-    hl.exec_cmd("hyprctl setcursor Bibata-Modern-Classic 24")
-    hl.exec_cmd("noctalia")
-    hl.exec_cmd("shelly-notifications")
-    hl.exec_cmd("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
-end)
